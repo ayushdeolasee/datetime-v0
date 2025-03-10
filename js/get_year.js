933ed4f9e51391c7ebda889741f99c9aff4d402c
Ayush Deolasee <pypdeveloper@deolasee.org>
2025-03-10 18:36:42 +0530

    modifying get_year and get_month function to use Math.floor

--- js/get_year.js
+++ js/get_year.js
@@ -1,5 +1,6 @@
 function get_year(value) {
     const i64_value = Number(value.get().toObject().value);
-    const date = new Date(i64_value / 1000000);
+    const i64_value_millis = Math.floor(i64_value / 1000000);
+    const date = new Date(i64_value_millis);
     return date.getFullYear();
 }
