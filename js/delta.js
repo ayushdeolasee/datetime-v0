8d8e8f582ed31eb7f92de5ff876e0708e3b9cf0b
Ayush Deolasee <pypdeveloper@deolasee.org>
2025-03-11 11:00:04 +0530

    adding delta function to index.ftd

--- js/delta.js
+++ js/delta.js
@@ -4,7 +4,7 @@ function delta(start, end = null) {
         const start_i64_value_millis = Math.floor(start_i64_value / 1000000);
         const start_date = new Date(start_i64_value_millis);
 
-        const delta = start_date - Date.now();
+        const delta = Math.floor((Date.now() - start_date) / 60000);
         return new fastn.recordInstanceClass({ value: delta });
     } else {
         const start_i64_value = Number(start.get().toObject().value);
@@ -15,7 +15,7 @@ function delta(start, end = null) {
         const end_i64_value_millis = Math.floor(end_i64_value / 1000000);
         const end_date = new Date(end_i64_value_millis);
 
-        const delta = end_date - start_date;
+        const delta = Math.floor((end_date - start_date) / 60000);
         return new fastn.recordInstanceClass({ value: delta });
     }
 }
