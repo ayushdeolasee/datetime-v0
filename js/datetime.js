51501402bfcc500b7e2247bc391915b556917b58
Ayush Deolasee <pypdeveloper@deolasee.org>
2025-03-12 15:47:19 +0530

    using  to load js file

--- js/datetime.js
+++
@@ -1,7 +0,0 @@
-function datetime(diff) {
-    const currentDate = new Date();
-    const date = new Date(currentDate.getTime() + diff * 60000);
-    console.log(date);
-    const now = BigInt(date) * BigInt(1000000);
-    return new fastn.recordInstanceClass({ value: now });
-}
